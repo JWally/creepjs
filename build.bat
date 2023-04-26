248b22271d9ef63b196762da3973158067a4095d
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2023-04-26 14:23:24 -0500

    updated to incorporate obfuscator into builds

---
+++ build.bat
@@ -0,0 +1,7 @@
+javascript-obfuscator voodoo.min.js --output voo.doo.min.js ^
+    --options-preset default ^
+    --simplify true ^
+    --self-defending true ^
+    & rename voodoo.min.js voodoo.min.mjs ^
+    & rename voo.doo.min.js voo.doo.min.mjs
+    
