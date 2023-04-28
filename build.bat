ba8fe63698f2d475790e6e69ddb631ce715e2d7e
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2023-04-28 05:21:48 -0500

    make better

--- build.bat
+++ build.bat
@@ -5,5 +5,6 @@ javascript-obfuscator voodoo.min.js --output voo.doo.min.js ^
     & del voodoo.min.mjs ^
     & del voo.doo.min.mjs ^
     & rename voodoo.min.js voodoo.min.mjs ^
-    & rename voo.doo.min.js voo.doo.min.mjs
+    & rename voo.doo.min.js voo.doo.min.mjs ^
+    & copy .\voo.doo.min.mjs ..\keyri-voodoo\src\creepjs\voo.doo.min.mjs
     
